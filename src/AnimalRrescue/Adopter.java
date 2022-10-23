4e37fc13c7674e392a9b19cd569e8244993bdaab
Diana <andreea.di.anna@gmail.com>
2022-10-24 01:33:33 +0300

    tema lab 11

---
+++ src/AnimalRrescue/Adopter.java
@@ -0,0 +1,10 @@
+package AnimalRrescue;
+
+    public class Adopter{
+
+        String name = "Briana";
+        byte age = 12;
+        String gender = "girl";
+        String HairColor = "golden brown";
+    }
+
---
+++ src/AnimalRrescue/Dog.java
@@ -0,0 +1,30 @@
+package AnimalRrescue;
+
+public class Dog {
+
+    String name = "Perdita";
+    byte age = 3;
+    String breed = "dalmatian";
+    String color = "white with black spots";
+    int weight = 25;
+    String gender = "female";
+
+    public void eat(){
+        System.out.println("Eating, yum, yum!");
+            }
+public void sleep(){
+    System.out.println("Snoring ZZzzZzZzz");
+}
+public void speak(){
+    System.out.println("Barking Ham ham ham");
+}
+public void run(){
+    System.out.println("running...");
+}
+public void tailWagging(){
+    System.out.println("Wagging her tail");
+}
+}
+
+
+
